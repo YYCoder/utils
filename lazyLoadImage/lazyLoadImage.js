77efd7dc7169c0bc3a3777d70be5dd1795b3e18b
yuanye <yuanye@baijiahulian.com>
2017-03-03 12:29:53 +0800

    ..

--- lazyLoadImage/lazyLoadImage.js
+++ lazyLoadImage/lazyLoadImage.js
@@ -4,14 +4,31 @@
  * @date    2017-02-22
  */
 (function () {
-    
+    'use strict';
+    var lazyLoadImage = {};
+    var imgArr = [];
 
-    function init() {
-        alert('init !!!');
+    lazyLoadImage.init = function () {
+        console.log('init !');
+        var allImgs = document.querySelectorAll('img');
+        imgArr = Array.prototype.slice.call(allImgs, 0);
+        console.log(imgArr);
     }
 
-    return {
-        init: init
+    function check() {
+        for (var i = 0; i < imgArr.length; i++) {
+            var hasLoaded = !!imgArr[i].src;
+            var isIntoView = (imgArr[i].y < window.innerHeight + window.scrollY)
+                            && (imgArr[i].y > window.scrollY);
+            if (!hasLoaded && isIntoView) {
+                imgArr[i].src = imgArr[i].dataset['src'];
+            }
+        };
     }
+
+    window.lazyLoadImage = lazyLoadImage;
+    window.addEventListener('scroll', function () {
+        check();
+    });
 
 })();
