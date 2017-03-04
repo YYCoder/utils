fb08163789a342ce6c2ff16e63e200abe8a1310a
yuanye <yuanye@baijiahulian.com>
2017-03-04 11:30:28 +0800

    ..

--- lazyLoadImage/lazyLoadImage.js
+++ lazyLoadImage/lazyLoadImage.js
@@ -16,14 +16,51 @@
     }
 
     function check() {
+        var hasLoaded;
+        var isIntoView;
+        var position;
         for (var i = 0; i < imgArr.length; i++) {
-            var hasLoaded = !!imgArr[i].src;
-            var isIntoView = (imgArr[i].y < window.innerHeight + window.scrollY)
-                            && (imgArr[i].y > window.scrollY);
+            hasLoaded = !!imgArr[i].src;
+            position = getPosition(imgArr[i]);
+            // isIntoView = ((position.y < screen.availHeight + window.scrollY)
+            //               && (position.y > window.scrollY))
+            // 			  || ;
             if (!hasLoaded && isIntoView) {
                 imgArr[i].src = imgArr[i].dataset['src'];
             }
         };
+    }
+
+    /**
+     * 获取图片相对document左上角的距离
+     * @param  {DOM} img(必须) [图片元素]
+     * @return {Object}       [坐标对象]     
+     */
+    function getPosition(img) {
+        var position = {};
+        // getBoundingClientRect()方法所有元素都有,现代浏览器都已兼容,IE8也兼容
+        if (typeof img.getBoundingClientRect === 'function') {
+            position['left'] = img.getBoundingClientRect().left;
+            position['top'] = img.getBoundingClientRect().top;
+            position['right'] = img.getBoundingClientRect().right;
+            position['bottom'] = img.getBoundingClientRect().bottom;
+        }
+        else {
+            var elem = img;
+            position['top'] = 0;
+            position['bottom'] = elem.offsetHeight;
+            position['left'] = 0;
+            position['right'] = elem.offsetWidth;
+            // 若不支持getBoundingClientRect()方法,则只能循环offsetParent来获取绝对位置
+            while (elem.offsetParent !== null) {
+                position['left'] += elem.offsetLeft;
+                position['top'] += elem.offsetTop;
+                position['right'] += elem.offsetLeft;
+                position['bottom'] += elem.offsetTop;
+                elem = elem.offsetParent;
+            }
+        }
+        return position;
     }
 
     window.lazyLoadImage = lazyLoadImage;
