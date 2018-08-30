ad8f96b31391723d148046c9cb628bdecacab11c
yuanye <yuanye@qudian.com>
2018-08-30 11:35:28 +0800

    调整date.js单测

--- test/date.js
+++ test/date.js
@@ -6,29 +6,40 @@ const {
 function deepEq(actual, value, message) {
   assert.deepStrictEqual(actual, value, message)
 }
-function fail(message) {
-  assert.fail(message)
-}
 
 describe('日期方法：', () => {
+  function zeroFill(str) {
+    str += ''
+    str.length === 1 && (str = '0' + str)
+    return str
+  }
+
   describe('dateFormat: ', () => {
+    const now = Date.now()
+    const D = new Date
+    const res1 = `${D.getFullYear()}-${zeroFill(D.getMonth() + 1)}-${D.getDate()} ${zeroFill(D.getHours())}:${zeroFill(D.getMinutes())}:${zeroFill(D.getSeconds())}`
+    const res2 = `${D.getFullYear()}, ${zeroFill(D.getMonth() + 1)}, ${D.getDate()}`
+    const res3 = `${zeroFill(D.getHours())}.${zeroFill(D.getMinutes())}.${zeroFill(D.getSeconds())}`
+    const res4 = `${zeroFill(D.getHours())}:${zeroFill(D.getMinutes())}:${zeroFill(D.getSeconds())}`
+    const res5 = `${D.getDay()}`
+
     it('默认不传值使用', () => {
       assert.ok(dateFormat())
     })
-    it('传入时间戳 1534159114263，应返回 2018-08-13 19:18:34', () => {
-      deepEq(dateFormat(1534159114263), '2018-08-13 19:18:34')
+    it(`传入时间戳 ${now}，应返回 ${res1}`, () => {
+      deepEq(dateFormat(now), res1)
     })
-    it('传入时间戳 1534159114263，指定格式 %Y, %m, %d，应返回 2018, 08, 13', () => {
-      deepEq(dateFormat(1534159114263, '%Y, %m, %d'), '2018, 08, 13')
+    it(`传入时间戳 ${now}，指定格式 %Y, %m, %d，应返回 ${res2}`, () => {
+      deepEq(dateFormat(now, '%Y, %m, %d'), res2)
     })
-    it('传入时间戳 1534159114263，指定格式 %H.%M.%S，应返回 19.18.34', () => {
-      deepEq(dateFormat(1534159114263, '%H.%M.%S'), '19.18.34')
+    it(`传入时间戳 ${now}，指定格式 %H.%M.%S，应返回 ${res3}`, () => {
+      deepEq(dateFormat(now, '%H.%M.%S'), res3)
     })
-    it('传入时间戳 1534159114263，指定格式 %T，应返回 19:18:34', () => {
-      deepEq(dateFormat(1534159114263, '%T'), '19:18:34')
+    it(`传入时间戳 ${now}，指定格式 %T，应返回 ${res4}`, () => {
+      deepEq(dateFormat(now, '%T'), res4)
     })
-    it('传入时间戳 1534159114263，指定格式 %w，应返回 1', () => {
-      deepEq(dateFormat(1534159114263, '%w'), '1')
+    it(`传入时间戳 ${now}，指定格式 %w，应返回 ${res5}`, () => {
+      deepEq(dateFormat(now, '%w'), res5)
     })
   })
 })
